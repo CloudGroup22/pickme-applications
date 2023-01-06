41a31d2c196412dd26f2c0220fd756536668507d
buddhikasandaruwan <buddhikasandaruwanph@gmail.com>
2023-01-06 21:41:35 +0530

    Add order Handler

---
+++ order-service/src/main/java/org/pickme/OrderHandler.java
@@ -0,0 +1,47 @@
+package org.pickme;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Map;
+
+public class OrderHandler implements RequestHandler<Map<String,String>, String>{
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    @Override
+    public String handleRequest(Map<String,String> event, Context context)
+    {
+        LambdaLogger logger = context.getLogger();
+        String response = "200 OK\n";
+        //logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
+        logger.log("CONTEXT: " + gson.toJson(context));
+        logger.log("EVENT: " + gson.toJson(event));
+        logger.log("EVENT TYPE: " + event.getClass());
+
+        try {
+
+            // Connect to the database
+            Connection conn = DriverManager.getConnection("jdbc:mysql://pickmefood.cn4g5pawgjm1.us-east-1.rds.amazonaws.com:3306/pickmefood", "admin", "OgXqylVqq7LldFMq1tY8");
+
+            // Execute a query and print the result
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT full_name FROM deliverperson");
+            while (rs.next()) {
+                return rs.getString(1) + "\n";
+            }
+
+            // Close the connection
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+}
