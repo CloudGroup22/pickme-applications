6f20061c7519e9c6ebfe34f972d41f90cc813afc
Chathumika Alwis <chathumikaalwis@gmail.com>
2023-01-06 23:28:43 +0530

    db change and url parmeter support

--- restaurants-service/src/main/java/org/pickme/Handler.java
+++ restaurants-service/src/main/java/org/pickme/Handler.java
@@ -80,6 +80,7 @@ public class Handler implements RequestHandler<Map<String,String>, JsonObject>{
         logger.log("results: " + rs.getString(1));
         String resultString = "{ \"name\": "+rs.getString(1)+", \"description\": "+rs.getString(2)+", \"rating\": "+rs.getFloat(3)+"}";
         logger.log("results String: " + resultString);
+        logger.log("results String: " + resultString);
         JsonObject resultObject = new Gson().fromJson(resultString, JsonObject.class);
         return resultObject;
       }
