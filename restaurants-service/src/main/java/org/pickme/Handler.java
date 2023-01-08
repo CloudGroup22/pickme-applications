07c72c9e4487f43b6499d946b6090717fc39d787
chathumika@intervest.lk <chathumika@intervest.lk>
2023-01-08 09:52:46 +0530

    db configurable resturant variables

--- restaurants-service/src/main/java/org/pickme/Handler.java
+++ restaurants-service/src/main/java/org/pickme/Handler.java
@@ -42,7 +42,8 @@ public class Handler implements RequestHandler<Map<String,String>, String>{
     try {
 
       // Connect to the database
-      Connection conn = DriverManager.getConnection("jdbc:mysql://pickmefood.cn4g5pawgjm1.us-east-1.rds.amazonaws.com:3306/pickmefood?useSSL=false", "admin", "OgXqylVqq7LldFMq1tY8");
+//      Connection conn = DriverManager.getConnection("jdbc:mysql://pickmefood.cn4g5pawgjm1.us-east-1.rds.amazonaws.com:3306/pickmefood?useSSL=false", "admin", "OgXqylVqq7LldFMq1tY8");
+      Connection conn = DriverManager.getConnection("jdbc:mysql://"+System.getenv("DBHOST"), System.getenv("USERNAME"), System.getenv("PW"));
 
       // Execute a query and print the result
       String statementString = "SELECT name,description,rating,menu FROM Restaurant";
