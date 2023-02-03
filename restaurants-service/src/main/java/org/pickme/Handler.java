99569302f672df80412be4ac97f93b793e1ade5f
Fathima Dilhasha <dilhasha.uom@gmail.com>
2023-02-03 11:04:15 +0530

    Update description label

--- restaurants-service/src/main/java/org/pickme/Handler.java
+++ restaurants-service/src/main/java/org/pickme/Handler.java
@@ -78,7 +78,7 @@ public class Handler implements RequestHandler<Map<String,String>, String>{
       while (rs.next()) {
 //        System.out.println(rs.getString(1));
         logger.log("results: " + rs.getString(1));
-        resultString += "\"{ \"name\": \""+rs.getString(1)+"\", \"description\": \""+rs.getString(2)+"\", \"rating\": "+rs.getFloat(3)+"}\",";
+        resultString += "\"{ \"name\": \""+rs.getString(1)+"\", \"descriptionDetails\": \""+rs.getString(2)+"\", \"rating\": "+rs.getFloat(3)+"}\",";
         logger.log("results String: " + resultString);
         logger.log("results String: " + resultString);
         //JsonObject resultObject = new Gson().fromJson(resultString, JsonObject.class);
